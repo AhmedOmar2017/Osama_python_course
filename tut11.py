6ed403931a9aeeb7576b4169ef6c41b977d162b5
Ahmed Omar <eng.ahmed.omar2017@gmail.com>
2025-01-16 00:48:51 +0200

    tut11.py

--- tut11.py
+++ tut11.py
@@ -9,3 +9,26 @@ myStringTwo = " This double quote "
 
 print(myStringOne)
 print(myStringTwo)
+
+
+myStringThird = " This double quote 'TEST'" # To add quote in massage we switch between single and double quote
+myStringForth =' This single Quote "test"' # To add quote in massage we switch between single and double quote
+
+print(myStringThird)
+print(myStringForth)
+
+
+
+myStringFifth = """
+This 
+double
+quote 'TEST'
+"""
+myStringSixth = '''
+This 
+double
+quote \ "test"
+'''
+
+print(myStringFifth)
+print(myStringSixth)
